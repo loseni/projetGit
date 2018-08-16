571fba5d664c6c9fff48d95555c8e91c4e763140
loseni <losenidembele@yahoo.com>
2018-08-16 00:21:06 +0000

    exo drap and drop

--- index.php
+++ index.php
@@ -2,10 +2,24 @@
 <html>
 <head>
 	<title>Test git</title>
+	<style type="text/css">
+		.draggableBox
+		{
+			
+			position: absolute;
+			width: 80px; height: 60px;
+			padding-top: 10px;
+			text-align: center;
+			font-size: 40px;
+			background-color: #222;
+			color: #CCC;
+			cursor: move;
+		}
+	</style>
 </head>
 <body>
-<h1>Bonjour les amis</h1>
-<p>premier paragraphe de la page </p>
-<div>Encore une resolution d'un conflit</div>
+<div class="draggableBox">1</div>
+<div class="draggableBox">2</div>
+<div class="draggableBox">3</div>
 </body>
 </html>
