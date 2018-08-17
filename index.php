cebd75710462ec67016f007ead855238ccce9f7f
loseni <losenidembele@yahoo.com>
2018-08-17 10:01:38 +0000

    creation de formulaire d'inscription

--- index.php
+++ index.php
@@ -2,80 +2,29 @@
 <html>
 <head>
 	<title>Test git</title>
-	<style type="text/css">
-		.draggableBox
-		{
-
-			position: absolute;
-			width: 80px; height: 60px;
-			padding-top: 10px;
-			text-align: center;
-			font-size: 40px;
-			background-color: #222;
-			color: #CCC;
-			cursor: move;
-		}
-	</style>
+	
 </head>
 <body>
-		<div class="draggableBox">1</div>
-		<div class="draggableBox">2</div>
-		<div class="draggableBox">3</div>
+		<div class="container">
+			<form method="post" action="">
+				<div>
+					<label for="nom">Nom:</label>
+					<input type="text" name="nom">
+				</div>
 
-		<script type="text/javascript">
-			(function(){
+				<div>
+					<label for="prenom">Prenom:</label>
+					<input type="text" name="prenom">
+				</div>
 
-				var storage ={};
-
-				function addEvent(element,event,func)
-				{
-					if(element.attachEvent)
-					{
-						element.attachEvent("on"+event,func);
-					}
-					else
-					{
-						element.addEventListener(event,func,true);
-					}
-				}
-
-				function init()
-				{
-					var elements=document.getElementsByTagName("div");
-					var elementslength=elements.length;
-
-					for(var i=0;i<elementslength;i++)
-					{
-						if(elements[i].className==="draggableBox")
-						{
-							addEvent(elements[i],"mousedown",function(e){
-								var s = storage;
-								s.target = e.target || event.srcElement;
-								s.offsetX = e.clientX - s.target.offsetLeft;
-								s.offsetY = e.clientY - s.target.offsetTop;
-							   
-							});
-
-							addEvent(elements[i],"mouseup",function(e){
-								storage={};
-							});
-						}
-					}
-
-					addEvent(document,"mousemove",function(e){
-						var target=storage.target;
-						if(target)
-						{
-							target.style.top=e.clientY-storage.offsetY+"px";
-							target.style.left=eclientX-storage.offsetX+"px";
-						}
-					});
-
-				}
-
-				init();
-
-			})();
-		</script>
+				<div>
+					<label>Sexe:  </label>
+					<label for="m">M</label>
+					<input type="radio" name="sexe" id="m">
+					<label for="f">F</label>
+					<input type="radio" name="f">
+				</div>
+			</form>
+		</div>
 </body>
 </html>
